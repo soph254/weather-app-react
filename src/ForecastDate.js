313667c237d89842224564bf7809fa3d11970cd8
soph254 <sophiejones591@gmail.com>
2021-05-31 19:12:28 +0100

    removing duplicated code

--- src/ForecastDate.js
+++ src/ForecastDate.js
@@ -4,8 +4,7 @@ import "./ForecastDate.css";
 export default function ForecastDate() {
     return (
         <div className="col-2">
-            <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather Icon" className="forecast-icon"></img><br />
-            Mon 31st May <br /> <strong>21°C</strong>
+            Mon 31st May <br /> <strong>21°C | °F</strong>
         </div>
     );
 }
