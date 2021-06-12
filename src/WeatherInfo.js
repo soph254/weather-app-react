50e13157b414a4f6d4a6c00a4d8cef1564aa5682
soph254 <sophiejones591@gmail.com>
2021-06-12 17:40:43 +0100

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,39 +6,43 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
-    <div className="WeatherInfo">
-<div className="col-6 current-temperature">
+        <div className="WeatherInfo">
+            <div className="current-temperature">
                 <div className="current-temperature">
                     <h2>{props.data.city}</h2>
                     <FormattedDate date = {props.data.date} />
                     <div className="row">
-                        <div className="right-temperature-details">
+                        <div className="col-sm-6">
+                            <div className="right-temperature-details">
                             <div className="float-left">
-                            <WeatherIcon code={props.data.icon} />
+                                <WeatherIcon code={props.data.icon} />
+                                <h4 className="weatherDescription">{props.data.description}</h4>
                             </div>
                         </div>
-                        <div className="float-left">
-                            <WeatherTemperature celcius={props.data.temperature} />
-                            <span className="current-temp">{props.data.temperature}</span>
-                           
+                        <div className="col-sm-6">
+                            <div className="float-left">
+                                <WeatherTemperature celcius={props.data.temperature} />
+                                <span className="current-temp">{props.data.temperature}</span>  
+                            </div>
                         </div>
-                        <h4 className="weatherDescription">{props.data.description}</h4></div>
+                    </div>
+
+                            
+                            
+                        </div>
                         <div className="humidity">
                             Humidity: {props.data.humidity}
                             <br />
                             Wind: {props.data.wind}{" "}
-
-                            km/h
-                            {" "}
-                            | mph
+                            km/h {" "} mph
                         </div>
-                        </div>
+                    </div>
                 </div>
-            <div className="row forecast">
-                <Forecast />
-            </div> {" "}
+                <div className="row forecast">
+                    <Forecast />
+                </div> {" "}
             </div>
-            );
+    );
             
         
 }
